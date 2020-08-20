328c3dea20c59de02ab76ce9f2de532e5dbc443a
mfaibrahem <mfaibrahem2013@gmail.com>
2020-08-20 12:21:00 +0200

    update validation on click and validation on input change

--- index copy 2.js
+++ index copy 2.js
@@ -2,36 +2,57 @@ const UIstepperForm = document.querySelector('.stepper-form');
 const UIprevBtn = document.querySelector('.prev-btn');
 const UInextBtn = document.querySelector('.next-btn');
 const UIstepperSubmitBtn = document.querySelector('.stepper-submit-btn');
-const UIformSteps = document.querySelectorAll('.step');
+const UISteps = document.querySelectorAll('.step');
 
 let currentStep = 0;
-UInextBtn.addEventListener('click', handleNextClick);
-UIprevBtn.addEventListener('click', handlePrevClick);
-
+const stepErrorsObj = {};
 showStep();
-createStepperIndicators();
-selectCurrentStepIndicator();
 
 // select all current step inputs, textareas, and selectors
-const stepErrorsObj = {};
-let UIcurrentStep = document.querySelector('.active-step');
-let UIcurrentStepInputs = UIcurrentStep.querySelectorAll('.active-step input');
-let UIcurrentStepTextareas = UIcurrentStep.querySelectorAll(
+const UIcurrentStep = document.querySelector('.active-step');
+const UIcurrentStepInputs = UIcurrentStep.querySelectorAll('input');
+const UIcurrentStepTextareas = UIcurrentStep.querySelectorAll(
 	'.active-step textarea'
 );
-let UIcurrentStepSelectors = UIcurrentStep.querySelectorAll(
+const UIcurrentStepSelectors = UIcurrentStep.querySelectorAll(
 	'.active-step select'
 );
 
-const UIStepperInputs = document.querySelectorAll('.stepper-form input');
-const UIStepperTextareas = document.querySelectorAll('.stepper-form textarea');
-const UIStepperSelectors = document.querySelectorAll('.stepper-form select');
-// validate inputs on change for each form step
-validateInputFieldOnChange(UIStepperInputs, stepErrorsObj);
-validateInputFieldOnChange(UIStepperTextareas, stepErrorsObj);
-validateInputFieldOnChange(UIStepperSelectors, stepErrorsObj);
+createStepperIndicators();
+selectCurrentStepIndicator();
 
-function handleNextClick(e) {
+UInextBtn.addEventListener('click', e =>
+	handleNextClick(
+		UIcurrentStep,
+		UIcurrentStepInputs,
+		UIcurrentStepTextareas,
+		UIcurrentStepSelectors,
+		stepErrorsObj,
+		e
+	)
+);
+UIprevBtn.addEventListener('click', handlePrevClick);
+
+for (let singleStep of UISteps) {
+	handleSingleStep(singleStep);
+}
+
+function handleSingleStep(singleStep) {
+	console.log(UIcurrentStepInputs);
+	// validate inputs on change
+	validateInputFieldOnChange(UIcurrentStepInputs, stepErrorsObj);
+	validateInputFieldOnChange(UIcurrentStepTextareas, stepErrorsObj);
+	validateInputFieldOnChange(UIcurrentStepSelectors, stepErrorsObj);
+}
+
+function handleNextClick(
+	UIcurrentStep,
+	UIcurrentStepInputs,
+	UIcurrentStepTextareas,
+	UIcurrentStepSelectors,
+	stepErrorsObj,
+	e
+) {
 	e.preventDefault();
 	////////////////////////////////////////////////////
 	// validate inputs, selctors, textareas
@@ -40,24 +61,12 @@ function handleNextClick(e) {
 	validateInputFieldOnNextClick(UIcurrentStepSelectors, stepErrorsObj);
 	////////////////////////////////////////////////////
 	// do the following if and only if the current step is valid
-	if (
-		Object.keys(stepErrorsObj).length === 0 &&
-		currentStep < UIformSteps.length - 1
-	) {
+	if (Object.keys(stepErrorsObj).length === 0) {
 		// increment current step
-		// update the currentStep document selector
 		// show a step
 		// select indicator
 		currentStep += 1;
-		UIcurrentStep = document.querySelector(
-			`.step[data-step-index="${currentStep}"]`
-		);
-		UIcurrentStepInputs = UIcurrentStep.querySelectorAll('input');
-		UIcurrentStepTextareas = UIcurrentStep.querySelectorAll('textarea');
-		UIcurrentStepSelectors = UIcurrentStep.querySelectorAll('select');
-
-		currentStep === UIformSteps.length &&
-			(currentStep = UIformSteps.length - 1);
+		currentStep === UISteps.length && (currentStep = UISteps.length - 1);
 		showStep();
 		selectCurrentStepIndicator();
 	}
@@ -67,51 +76,27 @@ function handleNextClick(e) {
 	else {
 		for (let key in stepErrorsObj) {
 			const UIinputWithError = UIcurrentStep.querySelector(`[name=${key}]`);
-			if (UIinputWithError) {
-				UIinputWithError.parentElement.classList.add('has-error');
-				UIinputWithError.parentElement.querySelector(
-					'.helper-error'
-				).textContent = stepErrorsObj[key];
-			}
+			UIinputWithError.parentElement.classList.add('has-error');
+			UIinputWithError.parentElement.querySelector(
+				'.helper-error'
+			).textContent = stepErrorsObj[key];
 		}
-		// if (currentStep.length < UIformSteps.length)
-		// UIstepperSubmitBtn.setAttribute('disabled', true);
+
+		UIstepperSubmitBtn.setAttribute('disabled', true);
 		UInextBtn.setAttribute('disabled', true);
 	}
+
+	console.log(stepErrorsObj);
 }
 
 function handlePrevClick(e) {
 	e.preventDefault();
-	// delete all the errors object attributes
-	// remove the disabled from submit and next btns
-	// remove has-error from every input
 	// decrement current step
-	// update the current step document element
 	// show a step
 	// select indicator
-
-	for (let key in stepErrorsObj) {
-		delete stepErrorsObj[key];
-	}
-	if (UIcurrentStep.querySelector('.has-error')) {
-		for (let i of UIcurrentStep.querySelectorAll('.has-error')) {
-			i.classList.remove('has-error');
-			i.querySelector('.helper-error').textContent = '';
-		}
-	}
-
 	currentStep -= 1;
-	UIcurrentStep = document.querySelector(
-		`.step[data-step-index="${currentStep}"]`
-	);
-	UIcurrentStepInputs = UIcurrentStep.querySelectorAll('input');
-	UIcurrentStepTextareas = UIcurrentStep.querySelectorAll('textarea');
-	UIcurrentStepSelectors = UIcurrentStep.querySelectorAll('select');
-
 	currentStep === 0 && (currentStep = 0);
 	showStep();
-	// UIstepperSubmitBtn.removeAttribute('disabled');
-	UInextBtn.removeAttribute('disabled');
 	selectCurrentStepIndicator();
 }
 
@@ -123,7 +108,7 @@ function showStep() {
 		: (UIprevBtn.style.display = 'block');
 
 	// check if the current step is the equal to steps lenght - 1 (last step) => then remove the next btn and show the submit btn
-	if (currentStep === UIformSteps.length - 1) {
+	if (currentStep === UISteps.length - 1) {
 		UInextBtn.style.display = 'none';
 		UIstepperSubmitBtn.style.display = 'block';
 	} else {
@@ -132,7 +117,7 @@ function showStep() {
 	}
 
 	// check if the current step is the same of the stepIndex => then add the active-step class
-	for (let cs of UIformSteps) {
+	for (let cs of UISteps) {
 		cs.classList.remove('active-step');
 		const stepIndex = parseInt(cs.dataset.stepIndex);
 		if (currentStep === stepIndex) {
@@ -146,7 +131,7 @@ function createStepperIndicators() {
 	const indicators = document.createElement('div');
 	indicators.classList.add('stepper-indicators');
 	UIstepperForm.appendChild(indicators);
-	for (let i = 0; i < UIformSteps.length; i++) {
+	for (let i = 0; i < UISteps.length; i++) {
 		const indicator = document.createElement('div');
 		indicator.classList.add('indicator');
 		indicator.setAttribute('data-index', i);
@@ -179,12 +164,18 @@ function validateInputFieldOnNextClick(DOMcurrentStepInputs, errorsObj) {
 		}
 	}
 }
+
 ///////////////////////////
 // validate input on change
-function validateInputFieldOnChange(DOMstepperInputs, errorsObj) {
-	for (let input of DOMstepperInputs) {
+function validateInputFieldOnChange(DOMcurrentStepInputs, errorsObj) {
+	for (let input of DOMcurrentStepInputs) {
 		input.addEventListener(
-			'input',
+			'keyup',
+			e => handleInputFieldChange(errorsObj, input, e),
+			false
+		);
+		input.addEventListener(
+			'change',
 			e => handleInputFieldChange(errorsObj, input, e),
 			false
 		);
@@ -203,6 +194,9 @@ function handleInputFieldChange(errorsObj, input, e) {
 		// remove the has-error class from the parent of the input
 		// and clear the helper-error span
 		delete errorsObj[input.name];
+
+		// input.parentElement.classList.remove('has-error');
+		// input.parentElement.querySelector('.helper-error').textContent = '';
 	}
 
 	// if there are step errros handle them
@@ -212,25 +206,23 @@ function handleInputFieldChange(errorsObj, input, e) {
 	// disable the next and submit buttons
 	else if (!e.target.value && Object.keys(errorsObj.length > 0)) {
 		errorsObj[input.name] = 'Required';
+		// input.parentElement.classList.add('has-error');
+		// input.parentElement.querySelector('.helper-error').textContent =
+		// 	errorsObj[name];
 	}
 
 	if (Object.keys(errorsObj).length === 0) {
-		// UIstepperSubmitBtn.removeAttribute('disabled');
+		UIstepperSubmitBtn.removeAttribute('disabled');
 		UInextBtn.removeAttribute('disabled');
 
 		input.parentElement.classList.remove('has-error');
 		input.parentElement.querySelector('.helper-error').textContent = '';
 	} else {
-		// UIstepperSubmitBtn.setAttribute('disabled', true);
+		UIstepperSubmitBtn.setAttribute('disabled', true);
 		UInextBtn.setAttribute('disabled', true);
 		input.parentElement.classList.add('has-error');
 		input.parentElement.querySelector('.helper-error').textContent =
 			errorsObj[input.name];
 	}
+	console.log(errorsObj);
 }
-
-UIstepperForm.addEventListener('submit', e => {
-	// e.preventDefault();
-	console.log('hi');
-	handleNextClick(e);
-});
